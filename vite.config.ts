827fc1c79cd28e0cefa1f8a928779ef516fb76df
JohanSebastianPZ <sebastianmartinez@MacBook-Air-de-Sebastian.local>
2025-08-18 09:10:24 +0200

    Comfiguracion de despliegue en Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/black-canvas-webfolio/', // <--- Agrega esta línea
   server: {
     host: "::",
     port: 8080,
